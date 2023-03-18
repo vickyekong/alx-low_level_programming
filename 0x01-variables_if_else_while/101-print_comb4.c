be5c130192477ba0ef75555053e46e99d58cb08c
Victor Edemekong <vickyekong1@gmail.com>
2023-03-18 03:43:54 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,28 +5,22 @@
  * Return: Always 0
  */
 
-int main(void)
-{
-	int i;
-	int j;
-	int k;
-for (i = 0 ; i < 10 ; i++)
-{
-for (j = 1 ; j < 10 ; j++)
-{
-for (k = 2 ; k < 10 ; k++)
-{
-if (i < j && j < k);
-{
-putchar(i + '0');
-putchar(j + '0');
-putchar(k + '0');
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-putchar('\n');
-return (0);
+int main() {
+    int i = 0, j = 0, k = 0;
+
+    for(i = 0; i <= 7; i++) {
+        for(j = i + 1; j <= 8; j++) {
+            for(k = j + 1; k <= 9; k++) {
+                putchar(i + '0');
+                putchar(j + '0');
+                putchar(k + '0');
+                if(i != 7 || j != 8 || k != 9) {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+
+    return 0;
 }
