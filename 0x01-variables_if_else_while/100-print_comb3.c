e8e165407fd40cff0824d39007cdb7a19288a67b
Victor Edemekong <vickyekong1@gmail.com>
2023-03-18 07:14:41 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,13 +10,13 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i <= 7; i++)
+	for (i = 0; i <= 8; i++)
 	{
-		for (j = i + 1; j <= 8; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
-			if (i != 7 || j != 8)
+			if (i != 8 || j != 9)
 			{
 			putchar(',');
 			putchar(' ');
