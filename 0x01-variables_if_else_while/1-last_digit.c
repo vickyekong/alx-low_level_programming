768b0a8917cf89009db7187abc0dba8d307507ec
Victor Edemekong <vickyekong1@gmail.com>
2023-03-17 18:01:58 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	printf("Last digit of %d is %d and is 0\n", n, lastnum);
 	}
 	else if (lastnum < 6 && lastnum != 0)
-	{	
+	{
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastnum);
 	}
 	return (0);
