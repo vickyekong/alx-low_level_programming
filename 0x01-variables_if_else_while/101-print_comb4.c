9f5f162e2dcd313bc30c58ec1796d54949c07370
Victor Edemekong <vickyekong1@gmail.com>
2023-03-18 02:21:30 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,15 +16,15 @@ for (j = 1 ; j < 10 ; j++)
 {
 for (k = 2 ; k < 10 ; k++)
 {
-if (i < j && j < k)
-putchar(i + '0')
-putchar(j + '0')
-putchar(k + '0')
-putchar(',')
-putchar(' ')
+if (i < j && j < k);
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+putchar(',');
+putchar(' ');
 }
 }
 }
-putchar('\n')
+putchar('\n');
 return (0)
 }
