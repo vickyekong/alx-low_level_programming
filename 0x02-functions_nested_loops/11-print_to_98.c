934b2266f7b2ae193e1375b9f5f21c3f54351231
Victor Edemekong <vickyekong1@gmail.com>
2023-03-22 10:57:16 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-	for (; n < 98; n++)
+	for (; n <= 98; n++)
 	{
 	if (n == 98)
 	{
@@ -20,7 +20,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-	printf("%d", n);
+	printf("%d,", n);
 	}
 	}
 	}
@@ -36,7 +36,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-	printf("%d", n);
+	printf("%d,", n);
 	}
 	}
 	}
