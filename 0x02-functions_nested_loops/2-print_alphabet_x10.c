f0fcee02d0dec60b85b2a27ee582bedc2666ff5f
Victor Edemekong <vickyekong1@gmail.com>
2023-03-22 03:18:58 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,12 @@
 #include "main.h"
 /**
-* print_alphabet_×10 - Make alphabet ×10 times
+* print_alphabet_x10 - Make alphabet x10 times
 * Return: void
+*
 */
+
 void print_alphabet_x10(void)
+
 {
 	char c;
 
